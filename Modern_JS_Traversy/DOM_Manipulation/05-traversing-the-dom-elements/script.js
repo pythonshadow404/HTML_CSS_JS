aab07fb9fd9edb94d5b2fa527fff23dc0a9f0f0d
pythonshadow404 <pythonshadow404@gmail.com>
2023-11-11 20:50:01 -0600

    DOM-Elements

--- Modern_JS_Traversy/DOM_Manipulation/05-traversing-the-dom-elements/script.js
+++ Modern_JS_Traversy/DOM_Manipulation/05-traversing-the-dom-elements/script.js
@@ -1,1 +1,28 @@
 // everything in the DOM is a node
+
+let output;
+// Get child elements
+
+const parent = document.querySelector('.parent');
+
+output = parent.children;
+
+//parent.chilren is an array-like structure that is zero-based
+// access the second item with index of [1]
+output = parent.children[1].innerText; 
+output = parent.children[1].className;
+output = parent.children[1].nodeName;
+
+parent.children[1].innerText = 'Child Two';
+parent.children[1].style.color = 'red';
+
+parent.firstElementChild.innerText = 'Child One';
+parent.lastElementChild.innerText = 'Child Three';
+
+
+
+
+
+
+// console.dir(output);
+console.log(output);
