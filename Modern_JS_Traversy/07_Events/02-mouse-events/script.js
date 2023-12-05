3480b77a16648b4bc6d2366140016bf2e1945f65
pythonshadow404 <pythonshadow404@gmail.com>
2023-12-05 06:08:56 -0600

    Traversy: Create a mouse events(1) with addEventListener

--- Modern_JS_Traversy/07_Events/02-mouse-events/script.js
+++ Modern_JS_Traversy/07_Events/02-mouse-events/script.js
@@ -3,11 +3,22 @@ const logo = document.querySelector('img');
 
 //Place your functions before the event listeners
 const onClick = () => console.log('logo clicked');
-const onDoubleClick = () => console.log('double logo clicked');
+// 2. const onDoubleClick = () => console.log('double logo clicked');
+const onDoubleClick = () => {
+    if (document.body.style.backgroundColor !== 'purple') {
+        document.body.style.backgroundColor = 'purple'
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black'
+    }
+}
 
+const onRightClick = () => console.log('logo clicked');
 
+// Event Listeners
 //Place your event listeners at the bottom of the page
 logo.addEventListener('click', onClick);
 logo.addEventListener('dblclick', onDoubleClick);
-// logo.addEventListener('click', onClick); 
+logo.addEventListener('contextmenu', onRightClick); 
 // logo.addEventListener('click', onClick);
