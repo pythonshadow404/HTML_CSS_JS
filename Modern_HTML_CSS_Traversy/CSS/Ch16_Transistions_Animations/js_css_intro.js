9d698f22fb812eaf2b9a9ce9f5d025fac60ccb5a
pythonshadow404 <pythonshadow404@gmail.com>
2025-09-21 09:00:28 -0500

    add JavaScript features to a button

---
+++ Modern_HTML_CSS_Traversy/CSS/Ch16_Transistions_Animations/js_css_intro.js
@@ -0,0 +1,5 @@
+// To get the HTML button element assigned the variable 'button' we will need to use 
+// the JavaScript 'document object' in the browser environment.
+const button = document.getElementById('button');
+
+console.log(button); //We're gonna see our button with the ID of button and the class of btn and that has a bunch of properties and methods on it that we can do things with.
