66b19c13a6653dd284b90753b2ce9cbadb7d637e
pythonshadow404 <pythonshadow404@gmail.com>
2025-09-23 06:54:20 -0500

    "Add intro to JS eventListener and toggle class"

--- Modern_HTML_CSS_Traversy/CSS/Ch16_Transistions_Animations/js_css_intro.js
+++ Modern_HTML_CSS_Traversy/CSS/Ch16_Transistions_Animations/js_css_intro.js
@@ -3,11 +3,13 @@
 // const button = document.getElementById('button');
 const button = document.querySelector('#button');
 // or by the class: const button = document.querySelector('.btn');
+const heading = document.querySelector('h1');
 
-
-console.log(button); //We're gonna see our button with the ID of button and the class of btn and that has a bunch of properties and methods on it that we can do things with.
+// console.log(button); //We're gonna see our button with the ID of button and the class of btn and that has a bunch of properties and methods on it that we can do things with.
 
 // Now we want something to happen when we click this button. So it needs to have what we call an event listener. the button object has a method (methods have parentheses) we can attach to it.
 button.addEventListener('click', function() {
     document.body.classList.toggle('dark');
+    heading.classList.toggle('yellow');
+    
 });
