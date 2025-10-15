62225401f2277050da9a2f88f3a6b6b250233ffa
pythonshadow404 <pythonshadow404@gmail.com>
2025-10-14 20:51:14 -0500

    Add learn section

--- Modern_HTML_CSS_Traversy/CSS/Ch17_Tutor_Website_Project/js/script.js
+++ Modern_HTML_CSS_Traversy/CSS/Ch17_Tutor_Website_Project/js/script.js
@@ -17,12 +17,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //Change navbar background on scroll
 // ===========================
-// window.addEventListener('scroll', function() {
-//     const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
 
-//     if (window.scrollY > 0) {
-//         navbar.classList.add('navbar-scroll');
-//     } else { 
-//         navbar.classList.remove('navbar-scroll');
-//     }
-// })
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll');
+    } else { 
+        navbar.classList.remove('navbar-scroll');
+    }
+})
