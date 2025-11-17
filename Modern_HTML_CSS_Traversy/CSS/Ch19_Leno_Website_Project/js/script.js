ee1357e6ffb7f24d464bbc0b479b5e3dd5ca0948
pythonshadow404 <pythonshadow404@gmail.com>
2025-11-17 09:08:13 -0600

    Add navbar scroll class

--- Modern_HTML_CSS_Traversy/CSS/Ch19_Leno_Website_Project/js/script.js
+++ Modern_HTML_CSS_Traversy/CSS/Ch19_Leno_Website_Project/js/script.js
@@ -7,3 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
         mobileMenu.classList.toggle('active');
     });
 });
+
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+
+    if (this.window.scrollY > 0) {
+        navbar.classList.add('navbar--scroll');
+    } else {
+        navbar.classList.remove('navbar--scroll');
+    }
+})
